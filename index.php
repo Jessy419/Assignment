5cf6942c50996a8a6639f1c1d2deba5a22f8608b
Jessy419 <149263777+Jessy419@users.noreply.github.com>
2024-02-19 09:34:31 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$name = "Okoumeh Jessica Nnenna";
+$regNo = "2022/HND/35493/CS";
+$stateOfOrigin = "Ebonyi";
+$localGovt = "Afikpo North";
+$age = 26;
+
+echo "Name: $name<br>";
+echo "Registration Number: $regNo<br>";
+echo "State of Origin: $stateOfOrigin<br>";
+echo "Local Government Area: $localGovt<br>";
+echo "Age: $age years old";
+?>
